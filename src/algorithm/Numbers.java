ab00ec1b7eba385bfa81f761af1391936c295fbb
fazimi1 <fazliazimi@gmail.com>
2019-03-03 15:27:49 -0500

    update

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -62,7 +62,7 @@ public class Numbers {
 	public static void randomize( int arr[], int n)
 	{
 		Random r = new Random();
-		
+
 		for (int i = n-1; i > 0; i--) {
 			int j = r.nextInt(i);
 			int temp = arr[i];
